ef0ed63d4fa315421e03a1a6b9fa7ca7098cfe9c
Luis Martinez <luismartinez@macbookpro.attlocal.net>
2026-02-12 22:16:52 -0800

    Updated styling and some content

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav__link");
+const navToggle = document.querySelector('.nav-toggle');
 
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
 });
-
-navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
-  });
-})
